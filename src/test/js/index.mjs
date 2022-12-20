b1b3b6942027770013014983893429882bef6ef0
Anton Golub <antongolub@antongolub.com>
2022-12-20 15:14:00 +0300

    tests: separate bundle and unit tests

--- src/test/js/index.mjs
+++ src/test/js/index.mjs
@@ -1,8 +1,15 @@
-import { factory } from '../../../target/bundle/primitive-storage.mjs'
+import '@jest/globals'
+import { factory } from '../../../target/es6/index.mjs'
 
 describe('index', () => {
   it('exposes factory', () => {
     expect(factory).toEqual(expect.any(Function))
+    const storage = factory()
+
+    storage.set('foo', 'bar')
+    expect(storage.get('foo')).toEqual('bar')
+
+    storage.compact.cancel?.()
   })
 })
 
